ab4e099708e7efb0b45ec6ed41182321371cbebc
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-19 12:28:47 +0530

    Added computation logic4

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -7,4 +7,5 @@ read -p "Enter value for c" c
 compute1=$(( $a + $b * $c ))
 compute2=$(( $a * $b + $c ))
 compute3=$(( $c + $a / $b ))
-echo $compute1 $compute2 $compute3
+compute4=$(( $a % $b + $c ))
+echo $compute1 $compute2 $compute3 $compute4
