32f624add59323029282ea35743ba99ecca51b5c
Tejeswar0475 <tejeswar096@gmail.com>
2020-11-19 15:10:23 +0530

    Added loop for storing into dictionary

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -11,3 +11,18 @@ dict[2]=$(( $c + $a / $b ))
 dict[3]=$(( $a % $b + $c ))
 echo "result=" ${dict[@]}
 echo "count of dictionary values" ${#dict[@]}
+
+for (( i = 0 ; i<${#dict[@]} ; i++ ))
+do
+        arr[i]=${dict[$i]}
+        echo "check for dict values store in array arr[$i]" ${arr[$i]}
+done
+
+
+
+
+
+
+
+
+
